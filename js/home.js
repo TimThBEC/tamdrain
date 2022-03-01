30c4b0d6b3002b4cf4e67ea55b2bf201eb052202
TimTh <tim@bigenginecreative.org>
2022-03-01 07:55:03 -0800

    initial pages

---
+++ js/home.js
@@ -0,0 +1,17 @@
+// Tam Drain Studios Home Page
+
+// Init
+
+// Page connections
+
+var version = 1;
+
+$(document).ready(function () {
+
+  console.log("home.js v" + version);
+
+  
+}); // End doc ready
+
+// Functions
+
