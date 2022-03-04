7f3f0a1abbae2a7253813a5a5a7e3e5398e10021
TimTh <tim@bigenginecreative.org>
2022-03-03 20:06:15 -0800

    1.0

--- js/home.js
+++ js/home.js
@@ -2,16 +2,85 @@
 
 // Init
 
-// Page connections
+gsap.registerPlugin(ScrollTrigger);
 
-var version = 1;
+var version = 1.0;
+
+var devToolsOn = false; // Set to true to turn on GSAP Dev Tools.
+
+var gsapDevToolsContainer = "#gsap-dev-tools-js"; // Container for GSAP Dev Tools
+
+var introScroll = ".scroll-js";
+
+var heroLayer1 = "#hero-layer-1-js";
+var heroLayer2 = "#hero-layer-2-js";
+
+
+
+// Doc ready
 
 $(document).ready(function () {
 
   console.log("home.js v" + version);
 
-  
+  gsapPrep();
+  gsapDevTools();
+
 }); // End doc ready
 
 // Functions
 
+function gsapDevTools() {
+
+  // Set up GSAP dev tools
+
+  if (devToolsOn) {
+    gsap.set(gsapDevToolsContainer, { display: "block" });
+    GSDevTools.create({ container: gsapDevToolsContainer });
+  }
+
+}
+
+function gsapPrep() {
+
+  //Breakpoints
+
+  if (window.matchMedia("(min-width: 992px)").matches) {
+    console.log("Desktop");
+
+  } else {
+    console.log("Mobile");
+
+  }
+
+  // Timelines that don't depend on breakpoints
+
+  introTLPrep();
+
+
+}
+
+function introTLPrep() {
+
+  // Topic 1 - Image Parallax
+
+  var introTL = gsap.timeline({
+    id: "intro",
+    scrollTrigger: {
+      trigger: introScroll,
+      start: "50% bottom",
+      end: "bottom top",
+      scrub: true,
+      markers: true
+    }
+  });
+
+  introTL.to(
+    heroLayer1,
+    {
+      opacity: 0
+    }
+  );
+
+
+}
